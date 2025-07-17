c3276eea3a1d75579623fc2317c523a0f814f1ee
Pawas Aggarwal <pawas.agrwl@gmail.com>
2025-07-17 09:41:58 +0530

    update 5.26 with comments

--- Session5/5_26.py
+++ Session5/5_26.py
@@ -1,14 +1,23 @@
 # series sum
 
+# Setting the start and of series
 start = 1
 end = 99
+
+# creating variables for loop
 numerator = start
 result = 0
 
+# entry controlled loop checking until numerator < 99
 while numerator < end:
+    
+    # update denominator at every iteration before calculation
     denominator = numerator + 2
+    
+    # calculate and add to result
     result += numerator / denominator
-    print (numerator, "/", denominator, "=", numerator / denominator)
+    
+    # update numerator for next iteration
     numerator += 2
 
 print (result)
