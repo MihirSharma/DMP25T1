9a8cc9435648289f059da026f1fd8ee039c6a02a
Pawas Aggarwal <pawas.agrwl@gmail.com>
2025-07-18 00:42:14 +0530

    add 5_32.py

---
+++ Session5/5_32.py
@@ -0,0 +1,23 @@
+# Take required inputs
+amount = int(input("Amount to be saved: "))
+rate = int(input("Annual Interest Rate: "))
+months = int(input("Number of months: "))
+
+# Set variable for loop to check until last month
+count = 0
+
+# Set variable to calculate final value
+final = 0
+
+while count < months:
+    # multiply amount by rate and set is as final   
+    final = amount*(1 + (rate/100)/12)
+    
+    # change amount to final + amount for next iteration
+    amount = final + 100
+    
+    # increment count
+    count += 1
+
+# print final value
+print ("After ", months, "months: ", final)
