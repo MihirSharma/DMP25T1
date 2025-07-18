1c020fda9f0188047de7b787ae57ea4897629f71
Pawas Aggarwal <pawas.agrwl@gmail.com>
2025-07-18 12:19:28 +0530

    update listing 4.6

--- 2025-07-18.py
+++ 2025-07-18.py
@@ -9,3 +9,11 @@ while (i < 11):
         j += 1
     print ()
     i += 1
+
+# multiplication using for
+
+for i in range (1,11):
+    for j in range (1,11):
+        print (i * j, end = " ")
+    print ()
+    
